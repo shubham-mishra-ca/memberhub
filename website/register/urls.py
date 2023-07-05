f145c4c091d8330c4635d5104cc05faaa3397c6f
Shubham Mishra <smishra.on.ca@gmail.com>
2023-07-05 15:47:52 -0400

    Added Class-based registration view and success page.

--- website/register/urls.py
+++ website/register/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.index, name='index'),
-    path('', views.reg_view, name='reg_view'),
+    path('register/', views.index, name='index'),   
+    path('',views.RegFormView.as_view(), name='reg_view'), 
+    path('reg_complete/', views.reg_complete, name='reg_complete'),
 ]
--- website/register/views.py
+++ website/register/views.py
@@ -12,3 +12,17 @@ def reg_view(request):
     context = {}
     context['form'] = RegForm()
     return render(request, 'reg.html', context)
+
+from django.views.generic.edit import FormView
+
+class RegFormView(FormView):
+    template_name = 'reg.html'
+    form_class = RegForm
+    success_url = 'reg_complete'
+
+    def form_valid(self, form):
+        print(form.data)
+        return super().form_valid(form)
+    
+def reg_complete(request):
+    return HttpResponse("Registration complete!")
